ce409968a12dca884c3b3309088eaf0bc0a2727c
sreeharij <sreeharij2003@gmail.com>
2024-08-16 12:07:01 +0530

    add more user controller functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,7 @@ const createUser = asyncHandler(async (req,res) => {
         createToken(res, newUser._id);
 
         res.status(201).json({
+            "Status" : "New User Created!",
             _id: newUser._id, 
             username: newUser.username,
             email: newUser.email,
@@ -47,6 +48,7 @@ const loginUser =  asyncHandler(async (req,res) => {
             createToken(res,existingUser._id);
 
             res.status(201).json({
+                "Status" : "Congrats You got logged in",
                 _id: existingUser._id, 
                 username: existingUser.username,
                 email: existingUser.email,
@@ -70,4 +72,94 @@ const logoutCurrentUser = asyncHandler(async (req,res) => {
     res.status(200).json({message: "Logged out successfully."});
 });
 
-export { createUser,loginUser,logoutCurrentUser };
+const getAllUsers = asyncHandler(async (req,res) => {
+    const users = await User.find({});
+    res.json(users);
+})
+
+const getCurrentUserProfile = asyncHandler(async (req,res) => {
+    const user = await User.findById(req.user._id);
+    if(user){
+        res.json(user);
+    }
+    else{
+        res.status(404);
+        throw new Error("User Not Found. ");
+    }
+});
+
+const updateCurrentUserProfile = asyncHandler(async (req,res) => {
+    const user = await User.findById(req.user._id);
+
+    if(user){
+        user.username = req.body.username || user.username;
+        user.email = req.body.email || user.email;
+        
+        if(req.body.password){
+            const salt  = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            user.password = hashedPassword;
+        }
+
+        const updatedUser = await user.save();
+        res.json(user);
+    }
+    else{
+        res.status(404);
+        throw new Error("User Not Found. ");
+    }
+});
+
+const deleteUserById = asyncHandler(async (req,res) => {
+    const user = await User.findById(req.params.id);
+
+    if(user){
+        if(user.isAdmin){
+            res.status(400);
+            throw new Error("Cannot delete admin User.");
+        }
+
+        await User.deleteOne({_id: user._id});
+        res.json({message : "User Removed. "});
+    }
+    else{
+        res.status(404);
+        throw new Error("User Not Found. ");
+    }
+});
+
+const getUserById = asyncHandler(async (req,res) => {
+    const user = await User.findById(req.params.id);
+
+    if(user){
+        res.json(user);
+    }
+    else{
+        res.status(404);
+        throw new Error("User Not Found. ");
+    }
+});
+
+const updateUserById = asyncHandler(async (req,res) => {
+    const user = await User.findById(req.params.id);
+    
+    if(user){
+        user.username = req.body.username || user.username;
+        user.email = req.body.email || user.email;
+        user.isAdmin = Boolean(req.body.isAdmin);
+
+        const updatedUser = await user.save();
+        res.json({
+            _id : updatedUser._id,
+            username : updatedUser.username,
+            email : updatedUser.email,
+            isAdmin : updatedUser.isAdmin,
+        })
+    }
+    else{
+        res.status(404);
+        throw new Error("User Not Found. ");
+    }
+});
+
+export { createUser,loginUser,logoutCurrentUser,getAllUsers, getCurrentUserProfile, updateCurrentUserProfile, deleteUserById, getUserById, updateUserById};
